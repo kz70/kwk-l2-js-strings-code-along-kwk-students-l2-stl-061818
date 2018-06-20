d57f77a07fa518f676d92920048dbe336ab84182
Kailin Zhang <kailin11511@gmail.com>
2018-06-20 20:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your code in this file!
-
-const currentUser = 'Grace Hopper';
+ 
+ const currentUser = 'Grace Hopper';
  
 const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
+ 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
